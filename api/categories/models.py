27e36af77dc1d54e1201d10b2aad8628233e423a
Void <66706132+agazevedo@users.noreply.github.com>
2026-03-24 18:24:56 -0300

    feat(api): add type to category

--- api/categories/models.py
+++ api/categories/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
+    class Type(models.TextChoices):
+        CUSTEIO = 'custeio', 'Custeio'
+        CAPITAL = 'capital', 'Capital'
+    
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    type = models.CharField(max_length=50, choices=Type.choices)
 
     def __str__(self):
         return self.name
